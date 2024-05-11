d7be428b000f5e433b8fc31bc59dbfccd1a06ead
anoban <2016sb117@univ.jfn.ac.lk>
2024-05-11 10:27:17 +0530

    new file:   anoban/Classification.ipynb 	modified:   anoban/Feature Scaling.ipynb 	modified:   anoban/scale.py

--- anoban/scale.py
+++ anoban/scale.py
@@ -2,6 +2,7 @@
 # implementing following the sklearn scalers
 
 import numpy as np
+import pandas as pd
 from numpy.typing import NDArray
 from typing import override
 
@@ -10,10 +11,11 @@ class MaxDivideScaler(object):
     """ """
 
     def __init__(
-        self, copy: bool = True, feature_range: tuple[float, float] = (0.00, 1.000)
+        self, copy: bool = True, range_max: float = 1.000
     ) -> None:
         self.copy: bool = copy
-        self.feature_range: tuple[float, float] = feature_range
+        self.range_max: float = range_max
+        self.is_fitted: bool = False
 
     @override
     def __repr__(self) -> str:
@@ -21,10 +23,13 @@ class MaxDivideScaler(object):
 
     def fit(self, data: NDArray[np.floating | np.integer] | pd.DataFrame) -> None:
         """ """
+        self.is_fitted = True
+        self.cols_max = data.max(axis = 0)
         pass
 
-    def fit_transform(self) -> NDArray[np.floating | np.integer]:
+    def fit_transform(self, data: NDArray[np.floating | np.integer] | pd.DataFrame) -> NDArray[np.floating | np.integer]:
         """ """
+        self.is_fitted = True
         pass
 
     def transform(self) -> NDArray[np.floating | np.integer]:
@@ -33,7 +38,36 @@ class MaxDivideScaler(object):
 
 
 class MeanNormalizationScaler(object):
-    pass
+    """ """
+
+    def __init__(
+        self, copy: bool = True, range_max: float = 1.000
+    ) -> None:
+        self.copy: bool = copy
+        self.is_fitted: bool = False
+
+    @override
+    def __repr__(self) -> str:
+        pass
+
+    def fit(self, data: NDArray[np.floating | np.integer] | pd.DataFrame) -> None:
+        """ """
+        self.is_fitted = True
+        self.cols_mean = data.mean(axis = 0)
+        self.cols_min = data.min(axis = 0)
+        self.cols_max = data.max(axis = 0)
+
+
+    def fit_transform(self, data: NDArray[np.floating | np.integer] | pd.DataFrame) -> NDArray[np.floating | np.integer]:
+        """ """
+        self.is_fitted = True
+        pass
+
+    def transform(self) -> NDArray[np.floating | np.integer]:
+        """ """
+        if not is_fitted:
+            except In
+        pass
 
 
 class ZScoreScaler(object):
